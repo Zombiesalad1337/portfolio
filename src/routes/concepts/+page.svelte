c7592ac6f615e81cf0dc327f59efc82ef1f4e0aa
Rohit Choudhary <rohit01031999@gmail.com>
2024-05-19 00:54:44 +0530

    basic blog

---
+++ src/routes/concepts/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+	import BlogLayout from "$lib/components/blogLayout.svelte";
+    
+    import staticData from '$lib/staticData.json';
+
+    // let blogData = JSON.parse(staticData);
+
+</script>
+
+
+<BlogLayout blog={staticData.sections}>
+</BlogLayout>
