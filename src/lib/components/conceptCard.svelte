d5f9d7a35e163dbb25d1e12ce502420647ba2c2f
Rohit Choudhary <rohit01031999@gmail.com>
2024-07-15 01:01:59 +0530

    before img refactor

--- src/lib/components/conceptCard.svelte
+++ src/lib/components/conceptCard.svelte
@@ -11,10 +11,10 @@
   ];
 
   let currentImageIndex = 0;
-  let change = true;
+  let hover = false;
 
   function nextImage() {
-    if (change){
+    if (!hover){
         currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
     }
   }
@@ -34,10 +34,10 @@
   }
 
   function onMouseEnter(){
-    change = false;
+    hover = true;
   }
   function onMouseLeave(){
-    change = true;
+    hover = false;
   }
 
   // Start auto change on component mount
@@ -50,21 +50,21 @@
 </script>
 
 {#each [currentImageIndex] as index (index)}
-<div transition:fade class="image-container transition rounded-lg border-red border hover:bg-red hover:bg-opacity-10 text-white"
-     style="background-image: url({imageUrls[currentImageIndex]}); background-size: cover; background-position: center;"
+<div transition:fade class="image-container background-image: url({imageUrls[currentImageIndex]}); transition transition-hover-card rounded-lg  text-white {hover ? 'expand' : 'expand-revert'}"
+    style="background-image: url({imageUrls[currentImageIndex]});" 
      animate:fade="{{ duration: 3000 }}"
-     on:mouseover={onMouseEnter} on:mouseleave={onMouseLeave}>
+     on:mouseover={onMouseEnter} on:mouseleave={onMouseLeave} on:focus={onMouseEnter}>
   <div class="p-7">
-    <p class="text-sm text-white font-bold leading-[1.4] tracking-[0.04em]">
+    <p class="text-lg text-white font-bold leading-[1.4] tracking-[0.04em]">
       {type}
     </p>
 
     <div>
-      <p class="whitespace-pre-line text-lg font-medium mt-7 lg:text-3xl">{title}</p>
+      <p class="whitespace-pre-line text-2xl font-medium mt-7">{title}</p>
     </div>
 
-    <div>
-      <p class="whitespace-pre-line text-lg font-medium mt-7">{description}</p>
+    <div class="mb-40 transition-transform-description {hover ? 'visible-slide' : 'invisible-slide'}">
+    <p class="whitespace-pre-line text-base font-medium mt-7">{description}</p>
     </div>
 
   </div>
@@ -74,5 +74,31 @@
 <style>
   .image-container {
     transition: opacity 0.8s ease;
+    background-size: cover; 
+    background-position: center;
+  }
+    .transition-transform-description {
+    transition: transform 0.5s ease, opacity 0.5s ease;
+  }
+
+  .transition-hover-card {
+    transition: background-position 0.5s ease,translate 0.5s ease, scale 0.5s ease, blur 0.1s ease;
+  }
+  .expand {
+    background-position: bottom right;
+    translate: (500px, 500px);
+    scale: 1.05;
+  }
+  .expand-revert {
+    transform: translate(0, 0), scale(1.00);
+  }
+  .invisible-slide {
+    transform: translateX(+50px);
+    opacity: 0;
+  }
+
+  .visible-slide {
+    transform: translateX(+0px);
+    opacity: 1;
   }
 </style>
