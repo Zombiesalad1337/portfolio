4f2736caf6e547fd00a7d7ff1c038dac71f57b4c
Rohit Choudhary <rohit01031999@gmail.com>
2024-09-08 03:02:00 +0530

    theme toggle switch

--- src/lib/components/themeToggle.svelte
+++ src/lib/components/themeToggle.svelte
@@ -1,33 +1,54 @@
 <script lang="ts">
-    import { browser } from '$app/environment';
+	import { browser } from '$app/environment';
 
-    let darkMode = true;
+	let darkMode = true;
 
-    function handleSwitchDarkMode() {
-        darkMode = !darkMode;
+	function handleSwitchDarkMode() {
+		darkMode = !darkMode;
 
-        localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+		localStorage.setItem('theme', darkMode ? 'dark' : 'light');
 
-        darkMode
-            ? document.documentElement.classList.add('dark')
-            : document.documentElement.classList.remove('dark');
-    }
+		darkMode
+			? document.documentElement.classList.add('dark')
+			: document.documentElement.classList.remove('dark');
+	}
 
-    if (browser) {
-        if (
-            localStorage.theme === 'dark' ||
-            (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
-        ) {
-            document.documentElement.classList.add('dark');
-            darkMode = true;
-        } else {
-            document.documentElement.classList.remove('dark');
-            darkMode = false;
-        }
-    }
+	if (browser) {
+		if (
+			localStorage.theme === 'dark' ||
+			(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+		) {
+			document.documentElement.classList.add('dark');
+			darkMode = true;
+		} else {
+			document.documentElement.classList.remove('dark');
+			darkMode = false;
+		}
+	}
 </script>
 
-<button on:click={handleSwitchDarkMode}>
-  Toggle Theme
-</button>
+<div class="flex items-center justify-center">
+	<button
+		on:click={handleSwitchDarkMode}
+		class="relative h-8 w-16 rounded-full bg-gray-300 shadow-inner transition-all duration-300 ease-in-out {darkMode
+			? 'bg-dark'
+			: 'bg-light'} bg-cover bg-center"
+	>
+		<div
+			class="absolute left-0.5 top-0.5 h-7 w-7 rounded-full transition-transform duration-300 ease-in-out {darkMode
+				? 'translate-x-8 bg-[#707070]'
+				: 'translate-x-0 bg-white'}"
+		></div>
+	</button>
+</div>
 
+<style>
+	/* Background images for toggled and untoggled states */
+	.bg-light {
+		background-image: url('/blog-2.jpg');
+	}
+
+	.bg-dark {
+		background-image: url('/blog-1.jpg');
+	}
+</style>
