cc19e8dcfb00b1b3be5f7312c85783047c4fe0d0
Zombiesalad1337 <rohit01031999@gmail.com>
2024-09-17 17:14:55 +0530

    path aliases

--- svelte.config.js
+++ svelte.config.js
@@ -21,16 +21,12 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		vite: { // Add `vite` property here
-			resolve: {
-				alias: {
-					$components: path.resolve('src/lib/components'),
-					$lib: path.resolve('src/lib'),
-					$icons: path.resolve('static/icons'),
-					$images: path.resolve('static/images'),
-					$fonts: path.resolve('static/fonts')
-				}
-			}
+		alias: {
+			$components: path.resolve('src/lib/components'),
+			$lib: path.resolve('src/lib'),
+			$icons: path.resolve('static/icons'),
+			$images: path.resolve('static/images'),
+			$fonts: path.resolve('static/fonts')
 		}
 	}
 };
