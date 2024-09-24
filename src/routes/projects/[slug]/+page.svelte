497c5e094e41862a2dfad74129c9abca71500eca
Rohit Choudhary <rohit01031999@gmail.com>
2024-09-24 23:46:50 +0530

    projects structure

---
+++ src/routes/projects/[slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Footer from '$components/footer.svelte';
+	import Navbar from '$components/navbar.svelte';
+</script>
+
+<div class="bg-black">
+	<Navbar showAtScrollYMultiplier={0} initiallyVisible={true}></Navbar>
+	<Footer></Footer>
+</div>
