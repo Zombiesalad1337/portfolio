f21388476a6af8f4ce5a0417fb51856fa63d5d78
Rohit Choudhary <rohit01031999@gmail.com>
2024-09-28 18:53:59 +0530

    project responsibilites

---
+++ src/lib/components/projectResponsibilites.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let content: { heading: string; text: string }[];
+</script>
+
+<div>
+	{#each content as item}
+		<h3 class="font-pavelt text-4xl text-red">{item.heading}</h3>
+		<p class="text-3.5xl text-white">{item.text}</p>
+	{/each}
+</div>
--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -2,6 +2,7 @@
 	import Footer from '$components/footer.svelte';
 	import Navbar from '$components/navbar.svelte';
 	import ProjectBrandMarquee from '$components/projectBrandMarquee.svelte';
+	import ProjectResponsibilites from '$components/projectResponsibilites.svelte';
 	import ProjectStaticBanner from '$components/projectStaticBanner.svelte';
 	import LetsConnect from '$lib/letsConnect.svelte';
 
@@ -23,6 +24,21 @@
 	]; // Array of lines with optional styles
 	let description: string =
 		'Chainlink, MDU blocks, custom printed strapping, OSB, and oversized campaign imagery bringing the AF1 story to life in SoHo and the Grove.  Consumer engagement In-store tie dying activation.';
+
+	let content: { heading: string; text: string }[] = [
+		{
+			heading: 'Responsibilities',
+			text: 'Production oversight, Installation/Implementation Oversight, Partner Oversight, Budget oversight.'
+		},
+		{
+			heading: 'Collaborative Partners',
+			text: 'Nike Design, Nike Brand and Operations Teams'
+		},
+		{
+			heading: 'Timeline',
+			text: '8 Weeks'
+		}
+	];
 </script>
 
 <div class="bg-black">
@@ -30,10 +46,18 @@
 
 	<ProjectStaticBanner imageUrl={staticBannerUrl} {lines}></ProjectStaticBanner>
 
-	<ProjectBrandMarquee text={'NIKE'}></ProjectBrandMarquee>
-
 	<div class="mx-[10rem] my-[15rem] max-w-[90ch]">
 		<p class="text-5xl font-medium text-white">{description}</p>
+	</div>
+
+	<ProjectBrandMarquee text={'NIKE'}></ProjectBrandMarquee>
+
+	<div class="flex h-screen max-h-screen px-3.5rem">
+		<div class="w-1/2"></div>
+		<div class="w-1/2 flex-col items-center justify-start">
+			<ProjectResponsibilites {content}></ProjectResponsibilites>
+			<div class="h-full"></div>
+		</div>
 	</div>
 
 	<LetsConnect simplified={true}></LetsConnect>
