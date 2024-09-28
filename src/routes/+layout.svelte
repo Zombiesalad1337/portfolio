9aa52afb4aff19c44bce4dbf6c3410e7e3452ef0
Rohit Choudhary <rohit01031999@gmail.com>
2024-09-29 04:20:52 +0530

    removed view transition, too slow, find another way

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,50 @@
-<script>
-	import Bgbugtest from "$lib/components/bgbugtest.svelte";
-// import StarryBackground from "$lib/components/starryBackground.svelte";
+<script lang="ts">
+	import { onNavigate } from '$app/navigation';
+	import Bgbugtest from '$lib/components/bgbugtest.svelte';
+	// import StarryBackground from "$lib/components/starryBackground.svelte";
 
+	// onNavigate((navigation) => {
+	// 	if (document.startViewTransition) {
+	// 		return new Promise((resolve) => {
+	// 			document.startViewTransition(async () => {
+	// 				resolve();
+	// 				await navigation.complete;
+	// 			});
+	// 		});
+	// 	}
+	// });
 </script>
 
-<!-- <StarryBackground></StarryBackground> -->
-<!-- <Bgbugtest></Bgbugtest> -->
-
-<main>
-  <slot></slot>
+<main class="bg-black">
+	<slot></slot>
 </main>
 
 <style>
-    main {
-    position: relative;
-    z-index: 1; /* Ensure content is above the background */
-  }
+	main {
+		position: relative;
+		z-index: 1; /* Ensure content is above the background */
+	}
+
+	@keyframes fade-out {
+		to {
+			opacity: 0;
+		}
+	}
+
+	@keyframes slide-from-right {
+		from {
+			transform: translateX(100%);
+		}
+		to {
+			transform: translateX(0%);
+		}
+	}
+
+	:root::view-transition-old(root) {
+		animation: fade-out 200ms ease-out both;
+	}
+
+	:root::view-transition-new(root) {
+		animation: slide-from-right 200ms ease-out both;
+	}
 </style>
