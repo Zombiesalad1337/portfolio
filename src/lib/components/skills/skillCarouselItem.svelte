c715eca36990b1cd23b5115b8083cb09cee52f38
Rohit Choudhary <rohit01031999@gmail.com>
2024-10-12 01:53:30 +0530

    main page style

--- src/lib/components/skills/skillCarouselItem.svelte
+++ src/lib/components/skills/skillCarouselItem.svelte
@@ -4,6 +4,6 @@
 </script>
 
 <div class="flex flex-col justify-center items-center min-w-[2rem] gap-y-1rem">
-	<img src={img} class="w-6rem" />
+	<img src={img} class="h-8rem max-h-[8rem]" />
 	<p class="text-white text-xl">{name}</p>
 </div>
