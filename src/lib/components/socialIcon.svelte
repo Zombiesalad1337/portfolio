fcae72436d382f1902e49f9ec1bfcfa23d2f0d8b
Rohit Choudhary <rohit01031999@gmail.com>
2024-10-12 02:35:01 +0530

    social icons

---
+++ src/lib/components/socialIcon.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let src: string;
+	export let classes: string;
+	export let link: string;
+	export let srcDark: string;
+</script>
+
+<a href={link} target="_blank" rel="noopener noreferrer">
+	<img src={srcDark} class="{classes} filter invert brightness-100 w-1.5rem" />
+</a>
