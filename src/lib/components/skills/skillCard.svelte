ba6d3c89898efc654cd54f20f3aef308aeab6ded
Zombiesalad1337 <rohit01031999@gmail.com>
2024-10-14 23:30:06 +0530

    skill card base

---
+++ src/lib/components/skills/skillCard.svelte
@@ -0,0 +1,123 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+	import { goto } from '$app/navigation'; // Import SvelteKit's goto for programmatic navigation
+
+	export let type = 'Case Study';
+	export let name = 'Example Blog Post Title';
+	export let description =
+		'This is a brief description of the blog post. It flies in from the right on hover.';
+	let imgSrc = '/svelte.png';
+	export let slug: string;
+
+	let currentImageIndex = 0;
+	let hover = false;
+
+	function navigateToBlog(slug: string) {
+		goto(`/concepts/${slug}`); // Programmatically navigate to the blog's slug
+	}
+
+	// Automatically change image every x seconds
+
+	// Start auto change on component mount
+	import { onMount } from 'svelte';
+</script>
+
+<!-- TODO: concept card fonts -->
+{#each [currentImageIndex] as index (index)}
+	<div
+		transition:fade
+		class="image-container background-image: aspect-[4] w-full max-w-full cursor-pointer rounded-3xl url({imgSrc}); transition-hover-card text-white transition {hover
+			? 'expand'
+			: 'expand-revert'}"
+		on:click={() => navigateToBlog(slug)}
+	>
+		<div class="background-image" style="background-image: url({imgSrc});"></div>
+		<div class="content flex h-full flex-col justify-between px-1rem py-1rem font-neuemachina">
+			<div class="px-0.5rem py-2rem">
+				<div>
+					<p class="py-3rem text-right font-pavelt text-xl">{name}</p>
+				</div>
+			</div>
+
+			<div>
+				<div
+					class="transition-transform-description mt-auto pb-4rem {hover
+						? 'visible-slide'
+						: 'invisible-slide'}"
+				>
+					<p class="whitespace-pre-line text-right text-base font-medium">{description}</p>
+				</div>
+			</div>
+		</div>
+	</div>
+{/each}
+
+<style>
+	/* .image-container {
+    transition: opacity 0.8s ease;
+    background-size: cover; 
+    background-position: center;
+  } */
+	.image-container {
+		position: relative;
+		overflow: hidden;
+	}
+	.transition-transform-description {
+		transition:
+			transform 0.5s ease,
+			opacity 0.5s ease;
+	}
+
+	.transition-hover-card {
+		transition:
+			background-position 0.5s ease,
+			translate 0.5s ease,
+			scale 0.5s ease,
+			blur 0.1s ease;
+	}
+	.expand {
+		background-position: bottom right;
+		translate: (500px, 500px);
+		scale: 1.05;
+	}
+	.expand-revert {
+		transform: translate(0, 0), scale(1);
+	}
+	.invisible-slide {
+		transform: translateX(+50px);
+		opacity: 0;
+		transition-delay: 0s;
+	}
+
+	.visible-slide {
+		transform: translateX(+0px);
+		opacity: 1;
+		transition-delay: 0.3s;
+	}
+	.background-image {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-size: auto;
+		background-position: center;
+		filter: blur(0); /* Initially no blur */
+		background-origin: border-box;
+		transition:
+			background-position 0.5s ease,
+			translate 0.2s ease,
+			filter 0.5s ease;
+		transform: translate(0, 0);
+		transition-delay: 0.15s;
+	}
+	.content {
+		position: relative;
+		z-index: 1; /* Ensure content is above the background */
+	}
+	.image-container:hover .background-image {
+		filter: blur(5px); /* Apply blur on hover */
+		background-position: bottom -100px right;
+		transition-delay: 0s;
+	}
+</style>
---
+++ src/routes/skills/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import SkillCard from '$components/skills/skillCard.svelte';
+</script>
+
+<div class="px-3.5rem">
+	<div class="container-card my-16 grid grid-cols-12 justify-items-stretch gap-4rem">
+		{#each Array(12) as concept}
+			<div class="col-span-4">
+				<SkillCard></SkillCard>
+			</div>
+		{/each}
+	</div>
+</div>
