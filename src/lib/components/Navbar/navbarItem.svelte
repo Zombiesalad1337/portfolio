3b8ed2ba004e33da44c6639bab3d9d5da6588cbc
Zombiesalad1337 <rohit01031999@gmail.com>
2024-11-16 23:27:35 +0530

    panel more responsive, xs screen to tailwind conf

--- src/lib/components/Navbar/navbarItem.svelte
+++ src/lib/components/Navbar/navbarItem.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
 	import NavbarSvgIcon from './navbarSvgIcon.svelte';
 
+	export let className = '';
 	export let text: string;
 	export let link: string;
 	export let path: string;
+	export let hoverColor: string;
 </script>
 
 <a href={link} class="group">
 	<div
-		class="flex justify-start gap-x-0.5rem items-center transition-colors group-hover:text-secondary"
+		class="flex justify-start gap-x-0.5rem items-center transition-colors {hoverColor} {className}"
 	>
-		<NavbarSvgIcon {path} hoverColor="group-hover:text-secondary"></NavbarSvgIcon>
+		<NavbarSvgIcon {path} {hoverColor}></NavbarSvgIcon>
 		<div class="block md:hidden lg:block">
 			<p class="font-white ~/md:~text-base/xl">{text}</p>
 		</div>
