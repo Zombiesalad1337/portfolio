97d1d937c2a209caa28a52e4e854358538ee60c1
rohitChoudharyAlertEnterprise <rohit.choudhary@alertenterprise.com>
2024-12-09 12:20:33 +0530

    skill card before removing state

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -2,8 +2,8 @@
 	import SkillCard from '$components/skills/skillCard.svelte';
 </script>
 
-<div class="px-3.5rem">
-	<div class="container-card my-16 grid grid-cols-12 justify-items-stretch gap-4rem">
+<div class="~/md:~px-1rem/2rem ~/md:~py-1rem/2rem">
+	<div class="container-card grid md:grid-cols-12 grid-cols-4 justify-items-stretch md:gap-4rem ~/md:~gap-1rem/2rem">
 		{#each Array(12) as concept}
 			<div class="col-span-4">
 				<SkillCard></SkillCard>
