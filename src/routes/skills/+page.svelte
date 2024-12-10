05ec2549c871c8c9fff8108a6d56ec9776f1c96e
rohitChoudharyAlertEnterprise <rohit.choudhary@alertenterprise.com>
2024-12-10 14:58:47 +0530

    skills arranged

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
 	import SkillCard from '$components/skills/skillCard.svelte';
+	import staticData from '$lib/staticData.json';
 </script>
 
 <div class="~/md:~px-1rem/2rem ~/md:~py-1rem/2rem">
 	<div class="container-card grid md:grid-cols-12 grid-cols-4 justify-items-stretch md:gap-4rem ~/md:~gap-1rem/2rem">
-		{#each Array(12) as concept}
-			<div class="col-span-4">
-				<SkillCard></SkillCard>
-			</div>
+		{#each staticData.skillTypes as skillType}
+			<h2 class="col-span-4 md:col-span-12 text-white">{skillType.title}</h2>
+			{#each skillType.items as skill}
+				<div class="col-span-4">
+					<SkillCard {...skill}></SkillCard>
+				</div>
+			{/each}
 		{/each}
 	</div>
 </div>
