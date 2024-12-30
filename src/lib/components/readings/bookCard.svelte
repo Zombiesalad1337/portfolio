ba6ab26931a5622901841a527aa3a60a5217e9f6
Rohit Choudhary <rohit01031999@gmail.com>
2024-12-30 22:53:26 +0530

    30 dec

---
+++ src/lib/components/readings/bookCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let id: number;
+	export let title: string;
+	export let author: string;
+	export let description: string;
+	export let img: string;
+	export let type: string;
+</script>
+
+<div
+	class="flex w-full aspect-[4] bg-primary bg-opacity-25 justify-between ~/md:~h-[6.5rem]/[13rem]"
+>
+	<!-- text -->
+	<div class="grow"></div>
+	<!-- img -->
+	<div class="w-[30%] text-center">
+		<img src={img} class="object-cover w-full h-full bg-center" />
+	</div>
+</div>
---
+++ src/routes/readings/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import BookCard from '$components/readings/bookCard.svelte';
+	import SkillCard from '$components/skills/skillCard.svelte';
+	import staticData from '$lib/staticData.json';
+</script>
+
+<div class="~/md:~px-1rem/2rem ~/md:~my-[3rem]/[6rem]">
+	<div class="gap-y-1rem md:grid grid-cols-2 gap-x-1rem space-y-1rem md:space-y-0">
+		{#each staticData.readings.technicalDevelopment as book}
+			<BookCard {...book}></BookCard>
+		{/each}
+	</div>
+</div>
